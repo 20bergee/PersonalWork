af1ab5172699e14e884505a54154082b3e894552
Emily Berger <20bergee@elmbrookstudents.org>
2018-09-13 13:40:38 -0500

    NameAndClasses

---
+++ src/outputExamples/NameAndClasses.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class NameAndClasses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Emily Berger");
+System.out.println("Latin 3");
+System.out.println("AP Statistics");
+System.out.println("Honors Precalculus");
+System.out.println("AP Computer Science");
+	}
+
+}
