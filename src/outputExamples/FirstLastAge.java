eb6120d84416bf5a743623137e7eecc44f3c97db
Emily Berger <20bergee@elmbrookstudents.org>
2018-09-13 13:17:25 -0500

    FirstLastAge

---
+++ src/outputExamples/FirstLastAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstLastAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Emily");
+System.out.println("Berger");
+System.out.println(16);
+	}
+
+}
