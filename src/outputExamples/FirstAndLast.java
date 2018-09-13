11c186f64abf48527797025ade9a0d48b6a6f204
Emily Berger <20bergee@elmbrookstudents.org>
2018-09-13 13:10:53 -0500

    FirstAndLast

---
+++ src/outputExamples/FirstAndLast.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class FirstAndLast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Emily");
+System.out.println("Berger");
+	}
+
+}
