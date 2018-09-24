0cc3915bfda508d4da1cfc48a892da3bb2a21bcb
Emily Berger <20bergee@elmbrookstudents.org>
2018-09-24 14:15:04 -0500

    LockerCombo

---
+++ src/inputExamples/LockerCombo.java
@@ -0,0 +1,15 @@
+package inputExamples;
+import java.util.Scanner;
+public class LockerCombo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int numberone=45;
+int numbertwo=55;
+int numberthree=65;
+Scanner input=new Scanner (System.in);
+System.out.println("Locker Conmbination:");
+numberone=input.nextInt(); System.out.println("-"); numbertwo=input.nextInt();System.out.println("-");numberthree=input.nextInt();
+	}
+
+}
