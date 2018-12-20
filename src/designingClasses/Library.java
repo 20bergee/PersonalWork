51d403305b36ff10497b1bf56074261c15d404be
Unknown <20bergee@elmbrookstudents.org>
2018-12-20 14:12:23 -0600

    DesigningClassDay3

--- src/designingClasses/Library.java
+++ src/designingClasses/Library.java
@@ -3,3 +3,4 @@ package designingClasses;
 public class Library {
 
 }
+
---
+++ src/designingClasses/Student.java
@@ -0,0 +1,45 @@
+package designingClasses;
+import java.util.ArrayList;
+public class Student {
+private String name;
+private ArrayList <Integer> tests=new <Integer> ArrayList();
+private int howMany;
+public Student(){
+	name="Bob";
+			for(int i=0; i<5;i++){
+	tests.add(50);}
+	howMany=5;
+}
+public Student(String xName){
+name=xName;	
+}
+public void addaScore(int xScore){
+	if (xScore<0){
+		xScore=50;
+	}
+	tests.add(xScore);
+	howMany++;
+}
+public double averageScore(){
+	double total=0;
+	for(int i=0; i<tests.size(); i++){
+		total+=(double)tests.get(i);
+	}
+	double average=total/(double)tests.size();
+	return average;
+}
+public int lowestScore(){
+	int lowest=100;
+	for (int i=0; i<tests.size(); i++){
+		if (tests.get(i)<lowest){
+			lowest=tests.get(i);
+		}
+	}
+	return lowest;
+}
+public String toString(){
+	return ("The Student "+name+" has taken "+howMany+" tests");
+}
+public 
+
+}
