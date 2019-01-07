2b0e7709245328dfe9281aeddbe2d80f4d3f8b7f
Unknown <20bergee@elmbrookstudents.org>
2019-01-07 13:39:06 -0600

    InterfacesDay3

--- src/interfaces/Person.java
+++ src/interfaces/Person.java
@@ -1,5 +1,8 @@
 package interfaces;
 
 public interface Person {
-
+public void setName(String xName);
+public String getName();
+public int getAge();
+public String toString();
 }
---
+++ src/interfaces/TestSchool2.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public class TestSchool2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
