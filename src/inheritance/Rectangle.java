d72ab0560a8083921acb5940e14ea9655a64951c
Unknown <20bergee@elmbrookstudents.org>
2019-01-11 15:08:29 -0600

    Inheritance

--- src/inheritance/Rectangle.java
+++ src/inheritance/Rectangle.java
@@ -1,24 +1,13 @@
 package inheritance;
 
-public class Rectangle extends Quadrilateral{
+public class Rectangle extends Parallelogram{
 public Rectangle(){
-this.setS1(3.0);
-this.setS2(4.0);
-this.setS3(3.0);
-this.setS4(4.0);
+super();
 }
-public Rectangle(double xSide1, double xSide2, double xSide3, double xSide4){
-this.setS1(xSide1);
-this.setS2(xSide2);
-this.setS3(xSide3);
-this.setS4(xSide4);
+public Rectangle(double length, double width){
+ super(length, width, width);
 }
 public double area(){
-	if (this.getS1()!=this.getS2()){
-		return this.getS1()*this.getS2();
-	}
-	else{
-		return this.getS1()*this.getS3();
-	}
+ return super.getS1()*super.getS2();
 }
 }
