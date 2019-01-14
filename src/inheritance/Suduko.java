7e1f13eb0da2e13b5cac26d1c6bf44113e647f8d
Unknown <20bergee@elmbrookstudents.org>
2019-01-14 15:04:44 -0600

    Suduko

---
+++ src/inheritance/Suduko.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Suduko {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String suduko="sudoku";
+for (int i=0; i<4; i++){
+}
+	}
+
+}
